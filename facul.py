87d4c3ca2d8e70f1535c09079e2d4b6a14d2a5a6
kassiocarvalhojk <82849257+kassiocarvalhojk@users.noreply.github.com>
2021-09-08 14:02:01 -0300

    Update facul.py

--- facul.py
+++ facul.py
@@ -4,28 +4,22 @@ import time
 #ALGORITIMOS DE ORDENAÇÃO
 
 #BUBBLE SORT
-
 print('Algoritimo de ordenação BUBBLE SORT')
-inicio_BUBBLE = time.time()
 def bubble_sort(lista):
     for ultimo in range(len(lista), 0, -1):
         for atual in range(0,ultimo -1):
             if lista[atual] > lista[atual + 1]:
                 lista[atual], lista[atual + 1] = lista[atual + 1], lista[atual]
 
-
-a = list(range(101))
+a = list(range(21))
 random.shuffle(a)
 print('Lista gerada aleatoriamente\n', a)
 bubble_sort(a)
 print('Lista ordenada\n', a)
-final_BUBBLE = time.time()
-print('Tempo de execução do BUBBLE SORT: ', round(final_BUBBLE - inicio_BUBBLE,5),'segundos\n')
 
 
-
+#SELECTION SORT
 print('Algoritimo de ordenação SELECTION SORT')
-inicio_SELECTION = time.time()
 def selection_sort(lista):
     for item in range(0, len(lista)):
         menorValor = item
@@ -34,18 +28,15 @@ def selection_sort(lista):
                 menorValor = direita
         lista[item], lista[menorValor] = lista[menorValor], lista[item]
 
-
-b = list(range(101))
+b = list(range(21))
 random.shuffle(b)
 print('Lista gerada aleatoriamente\n', b)
 selection_sort(b)
 print('Lista ordenada\n', b)
-final_SELECTION = time.time()
-print('Tempo de execução do SELECTION SORT: ', round(final_SELECTION - inicio_SELECTION, 5),'segundos\n')
+
 
 #INSERCTION SORT
 print('Algoritimo de ordenação INSERCTION SORT')
-inicio_INSERCTION = time.time()
 def inserction_sort(lista):
     for posicao in range(0, len(lista)):
         elementoAtual = lista[posicao]
@@ -53,19 +44,15 @@ def inserction_sort(lista):
             lista[posicao] = lista[elementoAtual]
         posicao -= 1
 
-
-c = list(range(101))
+c = list(range(21))
 random.shuffle(c)
 print('Lista gerada aleatoriamente\n', c)
 selection_sort(c)
 print('Lista ordenada\n', c)
-final_INSERCTION = time.time()
-print('Tempo de execução do INSERCTION SORT: ', round(final_INSERCTION - inicio_INSERCTION, 5),'segundos')
 
 
 #MERGE SORT
 print('Algoritimo de ordenação MERGE SORT')
-inicio_MERGE = time.time()
 def merge_sort(lista):
     metade(lista, 0, len(lista)-1)
 
@@ -81,18 +68,15 @@ def metade(lista, inicio, fim):
 def merge(lista, inicio, fim):
     lista[inicio: fim + 1] = sorted(lista[inicio: fim + 1])
 
-
-d = list(range(101))
+d = list(range(21))
 random.shuffle(d)
 print('Lista gerada aleatoriamente\n', d)
 merge_sort(d)
 print('Lista ordenada\n', d)
-final_MERGE = time.time()
-print('Tempo de execução do MERGE SORT: ', round(final_MERGE - inicio_MERGE, 5),'segundos')
+
 
 #QUICK SORT
 print('Algoritimo de ordenação QUICK SORT')
-inicio_QUICK = time.time()
 def quick_sort(lista, esquerda, direita):
     if esquerda < direita:
         particao_posicao = particao(lista, esquerda, direita)
@@ -116,14 +100,49 @@ def particao(lista, esquerda, direita):
          
     return i
 
-e = list(range(101))
+e = list(range(21))
 random.shuffle(e)
 print('Lista gerada aleatoriamente\n', e)
 quick_sort(e, 0, len(e) - 1)
 print('Lista ordenada\n', e)
-final_QUICK = time.time()
-print('Tempo de execução do QUICK SORT: ', round(final_QUICK - inicio_QUICK, 5),'segundos')
 
+
+#HEAP SORT
+print('Algoritimo de ordenação HEAP SORT')
+def max_heap(lista, n, parente_index):
+    maior = parente_index
+    esquerda = 2*parente_index + 1
+    direita = 2*parente_index +2
+
+    if esquerda < n and lista[esquerda] > lista[maior]:
+        maior = esquerda
+    if direita < n and lista[direita] > lista[maior]:
+        maior = direita
+
+    if maior != parente_index:
+        lista[maior], lista[parente_index] = lista[parente_index], lista[maior]
+        max_heap(lista, n, maior)
+
+def heap_sort(lista):
+    n = len(lista)
+    for i in range(n // 2 - 1, -1, -1):
+        max_heap(lista, n, i)
+
+    for i in range(n-1, 0, -1):
+        lista[i], lista[0] = lista[0], lista[i]
+        max_heap(lista, i, 0)
+
+
+
+
+f = list(range(21))
+random.shuffle(f)
+print('Lista gerada aleatoriamente\n', f)
+heap_sort(f)
+print('Lista ordenada\n', f)
+
+
+#BUCKET SORT
 
 
 #ALGORITIMOS DE BUSCA
@@ -135,16 +154,36 @@ def busca_linear(arranjo, item):
             return arranjo.index(item)
 
         except ValueError:
-            return "Não existe"
+            return "não existe"
     else:
-        return "Não existe"
+        return "não existe"
 
 #PESQUISA DE UM ELEMENTO PRESENTE
 
-lista = list(range(1, 101))
+lista_busca_linear = list(range(1, 21))
 
-print("O elemento {} está no indice {} do arranjo!".format(55, busca_linear(lista, 55)))
+print("O elemento {} está no indice {} do arranjo!\n".format(15, busca_linear(lista_busca_linear, 15)))
 
 #PESQUISA DE UM ELEMENTO PRESENTE
 
-print("O elemento {} {} no arranjo!".format(200, busca_linear(lista, 200)))
+print("O elemento {} {} no arranjo!\n".format(200, busca_linear(lista_busca_linear, 200)))
+
+
+
+#BUSCA BINÁRIA
+def busca_binaria(lista, item, inicio, fim):
+    #if fim is None:
+        #fim = len(lista)-1
+    if inicio <= fim:
+        m = (inicio + fim) // 2
+        if lista[m] == item:
+            return m
+        if item < lista[m]:
+            return busca_binaria(lista, item, inicio, m-1)
+        else:
+            return busca_binaria(lista, inicio, m+1, fim)
+    return None
+
+
+lista_busca_binaria = list(range(21))
+print('O elemento da busca binária é ', busca_binaria(lista_busca_binaria, 10, 0, len(lista_busca_binaria)-1))
